521c24ab00925e8004e584bac6256d4126d33ffa
rocloudcc <137459673+rocloudcc@users.noreply.github.com>
2023-08-23 11:33:29 +0800

    Update tiktokdns.sh

--- tiktokdns.sh
+++ tiktokdns.sh
@@ -117,7 +117,7 @@ main() {
                     echo -e ""
                     echo -e ""
                 else
-                    echo "\e[1;32m该脚本适配于“\e[0m\e[1;31mCentos操作系统\e[0m\e[1;31m”\e[0m"
+                    echo -e "\e[1;31m该脚本适配于“\e[0m\e[1;31mCentos操作系统\e[0m\e[1;31m”\e[0m"
                     execute_sudo_silent update_interfaces
                 fi
             else
