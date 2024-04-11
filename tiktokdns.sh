7d474125329dfb5ff7df1bad9a914206c11b0327
rocloudcc <137459673+rocloudcc@users.noreply.github.com>
2024-04-11 23:28:49 +0800

    Update tiktokdns.sh

--- tiktokdns.sh
+++ tiktokdns.sh
@@ -2,31 +2,7 @@
 
 # 检测到的国家
 country=$(curl -s https://ipinfo.io/country)
-log_file="/var/log/change_dns.log"  # 日志文件路径
-
-# 设置日志输出
-exec > >(tee -i $log_file)
-exec 2>&1
-
-# 检查是否存在sudo命令
-if ! command -v sudo &> /dev/null; then
-    echo -e "\e[1;31m该脚本适配于“\e[0m\e[1;31mCentos操作系统\e[0m\e[1;31m”\e[0m"
-    exit 1
-fi
-
-# 隐藏执行结果的函数
-execute_silent() {
-    $@ &>/dev/null
-}
-
-# 隐藏输出并执行命令
-execute_sudo_silent() {
-    sudo -S $@ <<< "your_sudo_password_here" &>/dev/null
-}
-
-# 输出检测到的国家
-echo -e "\n\n\e[3;33m检测到的国家：\e[1;32m$country\e[0m ✅"
-echo -e "================================================"
+echo -e "\n\n\033[1;33m检测到的国家：\033[1;31m$country\033[0m ✅"
 
 # 定义 DNS 服务器
 declare -A dns_servers
@@ -37,95 +13,47 @@ dns_servers=(
     ["TH"]="61.19.42.5 8.8.8.8"
     ["ID"]="202.146.128.3 202.146.128.7 202.146.131.12"
     ["TW"]="168.95.1.1 8.8.8.8"
+    ["CN"]="111.202.100.123 101.95.120.109 101.95.120.106"
     ["HK"]="1.1.1.1 8.8.8.8"
     ["JP"]="133.242.1.1 133.242.1.2"
     ["US"]="1.1.1.1 8.8.8.8"
     ["DE"]="217.172.224.47 194.150.168.168"
-    ["AE"]="217.172.224.47 194.150.168.168"
     # ... 添加其他国家的DNS服务器
 )
 
-# 获取 /etc/resolv.conf 路径
-resolv_conf_path="/etc/resolv.conf"
-
-# 修改 /etc/resolv.conf
-update_resolv_conf() {
-    for dns_server in ${dns_servers[$country]}; do
-        echo "nameserver $dns_server" | sudo tee -a $resolv_conf_path.new
-    done
-    sudo mv $resolv_conf_path.new $resolv_conf_path
-}
-
-# 检查 /etc/resolv.conf 是否已更新为自定义的DNS
-check_custom_dns() {
-    local found_custom_dns=true
-    for dns_server in ${dns_servers[$country]}; do
-        if ! grep -q "nameserver $dns_server" "$resolv_conf_path"; then
-            found_custom_dns=false
-            break
-        fi
-    done
-    $found_custom_dns
+# 修改网络配置文件
+update_network_config() {
+    local interface_name="$1"
+    local dns_server_list="${dns_servers[$country]}"
+    echo -e "\033[1;34m执行任务：修改 /etc/sysconfig/network-scripts/ifcfg-$interface_name\033[0m"
+    # 解析 DNS1 和 DNS2
+    local dns1=$(echo $dns_server_list | cut -d ' ' -f 1)
+    local dns2=$(echo $dns_server_list | cut -d ' ' -f 2)
+    # 更新 DNS1 和 DNS2
+    sudo sed -i "s/^DNS1=.*/DNS1=$dns1/" /etc/sysconfig/network-scripts/ifcfg-$interface_name
+    sudo sed -i "s/^DNS2=.*/DNS2=$dns2/" /etc/sysconfig/network-scripts/ifcfg-$interface_name
 }
 
 # 重启 NetworkManager
 restart_network_manager() {
-    if command -v systemctl &>/dev/null; then
-        execute_sudo_silent systemctl restart NetworkManager
-    elif command -v service &>/dev/null; then
-        execute_sudo_silent service NetworkManager restart
-    fi
-}
-
-# 安装软件包（如果需要）
-install_package() {
-    if ! command -v $1 &>/dev/null; then
-        echo "正在尝试安装 $1 ..."
-        if command -v yum &>/dev/null; then
-            execute_sudo_silent yum install -y $1
-        elif command -v apt &>/dev/null; then
-            execute_sudo_silent apt install -y $1
-        else
-            echo "无法自动安装 $1，请手动安装。"
-            exit 1
-        fi
-    fi
+    sudo systemctl restart NetworkManager
 }
 
 # 主函数
 main() {
     case $country in
-        "PH"|"VN"|"MY"|"TH"|"ID"|"TW"|"HK"|"JP"|"US"|"DE"|"SG"|"AE")
-            install_package bind-utils dnsutils
-            update_resolv_conf
-            if check_custom_dns; then
-                execute_sudo_silent mv $resolv_conf_path.new $resolv_conf_path
-                restart_network_manager
-                if [ $? -eq 0 ]; then
-                    echo -e "\e[3;33m更新DNS成功\e[0m"
-                    echo -e "================================================"
-                    echo -e ""
-                    echo -e "\e[3;33m定制IPLC线路：\e[1;32mwww.rocloud.cc\e[0m"
-                    echo -e "\e[3;33mTG群聊：\e[1;31mhttps://t.me/rocloudiplc\e[0m"
-                    echo -e "\e[3;33m定制TIKTOK网络：\e[1;32m美国、泰国、越南、菲律宾等\e[0m"
-                    echo -e "\e[1;33m如有问题，请联系我：\e[1;35m联系方式TG：rocloudcc\e[0m"
-                    echo -e ""
-                    echo -e "================================================"
-                    echo -e ""
-                    echo -e ""
-                    echo -e "\e[1;32m DNS 已成功更换成目标国家：\e[1;31m$country\e[0m ✅"
-                    echo -e ""
-                    echo -e ""
-                else
-                    echo -e "\e[1;32m 可以查看一下节点DNS ：\e[1;31m是否成功更换为$country\e[0m ✅"
-                    execute_sudo_silent update_interfaces
-                fi
-            else
-                echo -e "修改DNS失败，未找到自定义DNS。"
-            fi
+        "PH"|"VN"|"MY"|"TH"|"ID"|"TW"|"CN"|"HK"|"JP"|"US"|"DE")
+            update_network_config "eth0" # 更改为您的网络接口名称
+            restart_network_manager
+            echo -e "\033[1;32m修改DNS成功，已重启NetworkManager。\033[0m"
+            echo -e "\n\n================================================\n"
+            echo -e "\033[3;33m定制IPLC线路：\033[1;32m广港、沪日、沪美、京德等\033[0m"
+            echo -e "\033[3;33m定制TIKTOK网络：\033[1;32m美国、泰国、越南、菲律宾等\033[0m"
+            echo -e "\n================================================\n\n"
+            echo -e "\033[1;32mNDS已成功更换成目标国家：\033[1;31m$country\033[0m ✅\n\n"
             ;;
         *)
-            echo -e "\e[1;32m该脚本适配于“\e[0m\e[1;31mCentos操作系统\e[0m\e[1;31m”\e[0m"
+            echo -e "\033[1;31m未识别的国家或不在列表中。\033[0m"
             exit 1
             ;;
     esac
